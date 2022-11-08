c535d7cfb3ea93576f8cec936c61a27400be3f7a
Javiera <69701833+javiesp@users.noreply.github.com>
2022-11-08 19:26:35 -0300

    LogIn-Java-v2
    Jframe
    JTextField
    Colecciones

---
+++ Java-GUI/LogIn/src/Registro.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Javi
+ */
+public class Registro {
+    private String user;
+    private String pass;
+
+    public Registro() {}
+
+    public Registro(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro: " + "\nuser: " + user + "\npass: " + pass ;
+    }
+           
+    
+    
+}
